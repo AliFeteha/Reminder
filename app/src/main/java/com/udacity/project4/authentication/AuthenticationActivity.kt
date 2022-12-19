03ee53d9091b4edbb4019a6edb6e637c515b2389
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2022-12-20 00:44:04 +0200

    listen the result of signIn if signing done send him ReminderActivity and start activity

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,11 +1,18 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -38,11 +45,22 @@ class AuthenticationActivity : AppCompatActivity() {
         )
 
     }
+//          Done: If the user was authenticated, send him to RemindersActivity
 
-//          TODO: If the user was authenticated, send him to RemindersActivity
-
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Log.i(Tag, "Successfully signed in user ")
+                Toast.makeText(this, "SignIn Successfully", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, RemindersActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Log.i(Tag, "Sign in unsuccessful")
+                Toast.makeText(this, "SignIn Unsuccessfully ", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
     }
 

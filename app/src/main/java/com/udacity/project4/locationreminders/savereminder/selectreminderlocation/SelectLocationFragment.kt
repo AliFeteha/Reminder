3a356d8a09efae444f6becd9f6e0b24dc7b39b25
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2022-12-22 00:00:07 +0200

    to immediately show the info window.

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -135,6 +135,7 @@ class SelectLocationFragment : BaseFragment() , OnMapReadyCallback{
                     .snippet(snippet)
 
             )
+            marker?.showInfoWindow()
         }
     }
 

93ac12dc4ff95dccf17cbdd6123b0bcd3220b51f
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2022-12-23 23:20:30 +0200

    make viewModel equal to class RemindersListViewModel and pass 1- ApplicationProvider.getApplicationContext to retrieve current application context. 2- remindersRepository which represent Fake data source.

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.udacity.project4.locationreminders.reminderslist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.udacity.project4.locationreminders.MainCoroutineRule
 import com.udacity.project4.locationreminders.data.FakeDataSource
@@ -31,5 +32,7 @@ class RemindersListViewModelTest {
         val reminder2 = ReminderDTO("alex","restaurant","KFC",2.5,3.5)
         val reminderList = mutableListOf(reminder1,reminder2)
         remindersRepository = FakeDataSource(reminderList)
+        viewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(), remindersRepository)
+
     }
 }

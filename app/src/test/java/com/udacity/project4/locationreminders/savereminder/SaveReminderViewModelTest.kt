b74cf105f46517806cdb1adcbb50c19e4e1bcd8f
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2022-12-24 00:24:29 +0200

    mainCoroutineRule which we create before

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -2,6 +2,7 @@ package com.udacity.project4.locationreminders.savereminder
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Rule
@@ -13,5 +14,7 @@ class SaveReminderViewModelTest {
     //Done: provide testing to the SaveReminderView and its live data objects
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
 }

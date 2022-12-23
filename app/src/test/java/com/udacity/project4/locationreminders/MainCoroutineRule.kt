a67be05fcb69be4601c2f5409cabe38d7b2e55a1
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2022-12-23 20:19:17 +0200

    TestCoroutineScope this give main courting rule the ability to control courtine timing using TestCoroutineDispatcher. starting function same as @before.

--- app/src/test/java/com/udacity/project4/locationreminders/MainCoroutineRule.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/MainCoroutineRule.kt
@@ -12,5 +12,10 @@ import org.junit.runner.Description
 @ExperimentalCoroutinesApi
 class MainCoroutineRule(val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()):
     TestWatcher(),
+    TestCoroutineScope by TestCoroutineScope(dispatcher) {
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
 
 }

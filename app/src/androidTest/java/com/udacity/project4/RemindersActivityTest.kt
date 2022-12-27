9c83e4b5180ea5901ac3dd87a3107d8642c96f5b
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2022-12-27 04:18:50 +0200

    click in selectLocation then hold in map to select location. then click in save location to save location . then click saveReminder to save it

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -9,8 +9,7 @@ import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -109,6 +108,11 @@ class RemindersActivityTest :
         closeSoftKeyboard()
         onView(withId(R.id.reminderDescription)).perform(typeText("restaurant"))
         closeSoftKeyboard()
+        onView(withId(R.id.selectLocation)).perform(click())
+        onView(withId(R.id.map)).perform(longClick())
+        onView(withId(R.id.save_location)).perform(click())
+        onView(withId(R.id.saveReminder)).perform(click())
+
 
     }
 }
